35936f1ba5b06562208b1737f0d77bbf05e3ccf8
texttechne <gedankengut@gmx.de>
2022-06-03 13:29:06 +0200

    feat(generator-ui5-react): i18n.config with correct determineLocale impl

--- packages/generator-ui5-react/src/app/templates/src/config/i18n.config.ts
+++ packages/generator-ui5-react/src/app/templates/src/config/i18n.config.ts
@@ -4,7 +4,18 @@ const FALLBACK_LOCALE = "<%= defaultLocale %>";
 
 export const createI18nConfig = (locale: string): I18nModuleRegistryOptions => ({
   debug: true,
-  determineLocale: () => locale,
+  determineLocale: (supportedLocales, fallbackLocale) => {
+    if (supportedLocales.includes(locale)) {
+      return locale;
+    }
+    if (/-/.test(locale)) {
+      const lang = locale.split("-")[0];
+      if (supportedLocales.includes(lang)) {
+        return lang;
+      }
+    }
+    return fallbackLocale;
+  },
   fallbackLocale: FALLBACK_LOCALE,
   supportedLocales: [FALLBACK_LOCALE],
   getTranslations: language => import(`../asset/locale/${language}.i18n.json`),
