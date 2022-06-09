261a35f3f6b045ceadbfeb820d0e6fc38027b96c
texttechne <gedankengut@gmx.de>
2022-06-09 13:46:59 +0200

    refactor(generator-ui5-react): introduce ui5.config and make craco.config generic

---
+++ packages/generator-ui5-react/src/app/templates/ui5.config.ts
@@ -0,0 +1,43 @@
+import { CracoUi5PluginOptions } from "@cpro-js/craco-plugin-ui5/src/craco-ui5-plugin";
+
+const config: CracoUi5PluginOptions = {
+  appId: "Z_YOUR_APP_ID",
+  appTitle: "<%= appTitle %>",
+  ui5Version: "1.84.14",
+  sandbox: {
+    theme: "sap_fiori_3",
+    tiles: [
+      {
+        // combination of semanticObject and action must be unique per item!
+        semanticObject: "<%= semanticObject %>",
+        action: "<%= actionName %>",
+        title: "<%= appTitle %>",
+        subtitle: "<%= appSubTitle %>",
+        info: "<%= appInfo %>",
+        icon: "sap-icon://<%= appIcon %>",
+        // parameters that will be always set (default) and provided as startup parameters
+        /*defaultParameters: {
+          myValue: "test",
+        },*/
+        // parameters by cross navigation
+        //additionalParameters: true,
+      },
+    ],
+  },
+  manifest: {
+    "sap.ui": {
+      fullWidth: true,
+    },
+    "sap.ui5": {
+      dependencies: {
+        minUI5Version: "1.76.0",
+      },
+      contentDensities: {
+        compact: true,
+        cozy: true,
+      },
+    },
+  },
+};
+
+export default config;
