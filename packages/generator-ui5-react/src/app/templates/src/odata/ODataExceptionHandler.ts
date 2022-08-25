4128231376895af39470a1c3174fae0ba9bd0008
texttechne <gedankengut@gmx.de>
2022-08-25 14:53:00 +0200

    fix(generator-ui5-react): missing ODataExceptionHandler

---
+++ packages/generator-ui5-react/src/app/templates/src/odata/ODataExceptionHandler.ts
@@ -0,0 +1,21 @@
+import { ErrorMessageRetriever } from "@odata2ts/axios-odata-client/lib/AxiosODataClient";
+
+interface SapError {
+  error: {
+    "@SAP__common.ExceptionCategory": string;
+    "@SAP__common.Timestamp": string;
+    "@SAP__common.TransactionId": string;
+    code: string;
+    message: string;
+  };
+}
+
+export const getErrorMessage: ErrorMessageRetriever = error => {
+  if (error.isAxiosError && error.response?.data) {
+    const sapError = error.response.data as SapError;
+    if (sapError.error.message) {
+      return sapError.error.message;
+    }
+  }
+  return error.message ?? error;
+};
