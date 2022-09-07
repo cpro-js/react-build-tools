67fbd47cfe5a70a90bf7f4bf90fd846e477fea3d
texttechne <gedankengut@gmx.de>
2022-09-07 15:54:59 +0200

    feat(generator-ui5-react): exception handler suitable for V2 and V4

--- packages/generator-ui5-react/src/app/templates/src/odata/ODataExceptionHandler.ts
+++ packages/generator-ui5-react/src/app/templates/src/odata/ODataExceptionHandler.ts
@@ -1,6 +1,6 @@
 import { ErrorMessageRetriever } from "@odata2ts/axios-odata-client";
 
-interface SapError {
+export interface SapErrorODataV4 {
   error: {
     "@SAP__common.ExceptionCategory": string;
     "@SAP__common.Timestamp": string;
@@ -10,12 +10,40 @@ interface SapError {
   };
 }
 
+export interface SapErrorODataV2 {
+  error: {
+    code: string;
+    message: {
+      lang: string;
+      value: string;
+    };
+  };
+}
+
 export const getErrorMessage: ErrorMessageRetriever = error => {
   if (error.isAxiosError && error.response?.data) {
-    const sapError = error.response.data as SapError;
-    if (sapError.error.message) {
-      return sapError.error.message;
+    let data = error.response.data as SapErrorODataV4 | SapErrorODataV2 | string;
+
+    if (typeof data === "string") {
+      let dataJson: SapErrorODataV4 | SapErrorODataV2 | undefined = undefined;
+      try {
+        dataJson = JSON.parse(data) as SapErrorODataV4 | SapErrorODataV2;
+      } catch (e) {}
+      if (dataJson != null) {
+        data = dataJson;
+      } else if (/.*<message>(.*)<\/message>.*/.test(data)) {
+        // it's XML, though => regexp parsing of message value
+        return data.replace(/.*<message>(.*)<\/message>.*/, "$1").trim();
+      }
+    }
+    if (typeof data !== "string") {
+      if (typeof data.error?.message === "string") {
+        return data.error.message;
+      } else if ((data as SapErrorODataV2).error?.message?.value) {
+        return (data as SapErrorODataV2).error.message.value;
+      }
     }
   }
+
   return error.message ?? error;
 };
