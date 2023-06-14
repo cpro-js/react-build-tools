d4826048c32e741f5ed3d4180ed5eb960fe65670
texttechne <gedankengut@gmx.de>
2023-06-14 12:18:50 +0200

    chore(generator-ui5-react): update odata2ts

--- packages/generator-ui5-react/src/app/templates/src/config/di.config.ts
+++ packages/generator-ui5-react/src/app/templates/src/config/di.config.ts
@@ -1,6 +1,6 @@
 import { Container, I18nService, createI18nModuleRegistry } from "@cpro-js/react-core";
 import { createNotificationRegistry } from "@cpro-js/react-ui5-notification";
-import { AxiosODataClient } from "@odata2ts/axios-odata-client";
+import { FetchClient } from "@odata2ts/http-client-fetch";
 // @ts-ignore: no typings available
 import { setLanguage } from "@ui5/webcomponents-base/dist/config/Language";
 
@@ -29,7 +29,7 @@ export const createContainer = async (options: {
 
   // initialize ODataService
   const odataConfig = createODataConfig(i18nService.getTranslationLocale(), options.resolveUri);
-  const odataClient = new AxiosODataClient(odataConfig);
+  const odataClient = new FetchClient(odataConfig);
   container.bindConstant(MainODataService, new MainODataService(odataClient, ODATA_SERVICE_PATHS.main));
 
   return container;
