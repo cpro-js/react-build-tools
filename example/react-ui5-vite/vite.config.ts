21a07a1453d754724c4052fb567c4e217f273c99
Jan-André Zinser <zinserjan@gmail.com>
2023-12-14 14:49:06 +0100

    refactor: react-scripts folder structure
    note: index.html must be part of src

--- example/react-ui5-vite/vite.config.ts
+++ example/react-ui5-vite/vite.config.ts
@@ -1,7 +1,11 @@
+import { resolve } from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: resolve(__dirname, "src"),
+  publicDir: resolve(__dirname, "public"),
   plugins: [react()],
 });
